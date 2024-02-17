02ff48b199f1a71c6df5e254567083f666e02147
Cindy Phan <c5phan@uwaterloo.ca>
2024-02-16 21:37:58 -0500

    comments

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# take photo/get photo
 
+# process photo/ image classifcation
+
+# output text 
+# prompt additional ingredients
+
+# if button is clicked, generate recipes using the database
+
+#
