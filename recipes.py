f3511f2c1d01e843b91ad3de37dc086c41754f23
Jana Habibi <janahabibi@ip-198-96-35-127.dynamic.yorku.ca>
2024-02-17 15:22:16 -0500

    recipes

--- recipes.py
+++ recipes.py
@@ -1,15 +1,17 @@
 import pymongo
 from pymongo import MongoClient
 
-# connectng db
+#connectng db
 client = MongoClient("mongodb+srv://kristinatruongkyt:H1p3uuoCy8p37d5F@cluster0.g81sekr.mongodb.net/fridgifyRecipeData")
 db = client.get_database('fridgifyRecipeData')
 records = db.Recipes
-print(records.count_documents({}))
 
-ingredients = ["vinegar"]
+#placeholder list of ingredients
+ingredients = ["1/2 c. vinegar"]
 
-pattern = ".*" + ingredients[0] + ".*"
+#we would loop/change the index
+pattern = "." + ingredients[0] + "."
 query = {'ingredients': {'$regex': pattern, '$options': 'i'}}
 
 print(list(records.find(query)))
+print("\n")
