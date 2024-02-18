6be71ac8d3d463c5b2aeeed8cce2ab94f4eac4e2
Cindy Phan <c5phan@uwaterloo.ca>
2024-02-17 21:22:06 -0500

    statics

--- ScanImage.py
+++ ScanImage.py
@@ -6,6 +6,15 @@ from gtts import gTTS
 from playsound import playsound
 from pymongo import MongoClient
 import api
+from flask import Flask, render_template, Response, url_for, redirect
+from cvlib.object_detection import draw_bbox
+
+vid = Flask(__name__)
+labels = []
+stop = 0
+
+def getLabels():
+    return labels
 
 # speech
 def speech(words):
@@ -17,21 +26,42 @@ def speech(words):
 # access camera
 def scan():
     video = cv2.VideoCapture(0)
-    labels = []
 
     while True:
-        ret, frame = video.read()
+        success, frame = video.read()
         bbox, label, conf = cv.detect_common_objects(frame) # box and label objects in video
         output_image = draw_bbox(frame, bbox, label, conf) # draw box
-        cv2.imshow("Object Detect", output_image) #window
+        ret, buffer = cv2.imencode('.jpg', output_image) #window
+        output_image = buffer.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + output_image + b'\r\n') 
         
         # items found
         for item in label: # adds to list if not already in it
             if item not in labels:
                 labels.append(item)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"): #click q to break out loop/window
+        if stop: # next button pressed to break out loop/window
             video.release()
+            cv2.destroyAllWindows()
             break
-    cv2.destroyAllWindows()
-    return labels
+
+@vid.route('/')
+def index():
+    return render_template('homedesktop.html')
+
+@vid.route('/camera', methods=['POST'])
+def camera():
+    return render_template('camera.html')
+
+@vid.route('/video_feed')
+def video_feed():
+    return Response(scan(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@vid.route('/end_video', methods=['POST'])
+def end_video():
+    stop = 1
+    return redirect(url_for('results')) 
+
+@vid.route('/results')
+def results():
+    return render_template('results.html')
--- main.py
+++ main.py
@@ -3,34 +3,23 @@ import recipes
 from playsound import playsound
 import threading
 
-# take photo/get photo
-def run_flask_app():
-    ScanImage.vid.run(debug=True)
-
-def main():
-    # Start the Flask app on a separate thread
-    flask_thread = threading.Thread(target=run_flask_app)
-    flask_thread.start()
-    ScanImage.vid.run(debug=True)
-    items = ScanImage.getLabels()
-
-    # filter items and find recipes
-    filtered = recipes.filterFood(items)
-    ingredients =  "Your ingredients are " + " ".join(filtered)
-    if (ingredients != "Your ingredients are "):
-        ScanImage.speech(ingredients)
-    recipeDict = recipes.getRecipes(filtered)
-    recipeNames = recipeDict[0]
-    recipeIngr = recipeDict[1]
-    recipeInstr = recipeDict[2]
+ScanImage.vid.run(debug=True)
+items = ScanImage.getLabels()
+# filter items and find recipes
+filtered = recipes.filterFood(items)
+ingredients =  "Your ingredients are " + " ".join(filtered)
+if (ingredients != "Your ingredients are "):
+    ScanImage.speech(ingredients)
+recipeDict = recipes.getRecipes(filtered)
+recipeNames = recipeDict[0]
+recipeIngr = recipeDict[1]
+recipeInstr = recipeDict[2]
     
-    #send recipe names to HTML to display
-    print(recipeNames)
+#send recipe names to HTML to display
+print(recipeNames)
 
 # when button click display the ingrident 
 
 # if button is clicked, generate recipes using the database
 
 #
-if __name__ == "__main__":
-    main()
